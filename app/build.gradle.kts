bc24350b475c915d2c821faa05dc52b2b271dc6b
Netanel Amar <NetanelCA2@gmail.com>
2025-08-31 10:04:52 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.netanel.smartdash"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.netanel.smartdash"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+            buildConfigField("String", "RAPID_API_KEY", "\"abe00e8bbamsha86872871166c98p19185fjsna884ebf6676c\"")
+            buildConfigField("String", "RAPID_API_HOST", "\"yahoo-weather5.p.rapidapi.com\"")
+        }
+
+        debug {
+            isMinifyEnabled = false
+            isDebuggable = true
+
+            buildConfigField("String", "RAPID_API_KEY", "\"abe00e8bbamsha86872871166c98p19185fjsna884ebf6676c\"")
+            buildConfigField("String", "RAPID_API_HOST", "\"yahoo-weather5.p.rapidapi.com\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
